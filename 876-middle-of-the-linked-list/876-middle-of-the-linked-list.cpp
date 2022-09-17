124da7f0cf141c7adc0c51a5ac759b772c86ffea
Abhijith J K <74914519+abhijith-jk@users.noreply.github.com>
2022-09-17 12:12:32 +0530

    Time: 0 ms (100.00%), Space: 7 MB (95.61%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
@@ -10,19 +10,17 @@
  */
 class Solution {
 public:
-    ListNode* middleNode(ListNode* head) {
-        ListNode *temp_length = head;   // temp node to count the length of ListNode
-        ListNode *temp = head;          // temp to traverse through ListNode from the given head node
-        int length = 0;
-        while(temp_length != NULL){
-            length++;                        // length keeps incrementing by 1 until temp_length reaches the end of ListNode
-            temp_length = temp_length->next; // traverses through ListNode until it reaches the end
+     ListNode* middleNode(ListNode* head) 
+    {
+        if(head == NULL)
+            return head;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast != NULL && fast -> next != NULL)
+        {
+            slow = slow -> next;
+            fast = fast -> next -> next;
         }
-        int counter = 0;                    // flag counter to make sure temp node stops traversing once it reaches the middle (length/2)
-        while(counter < length/2){
-            counter ++;
-            temp = temp-> next;
-        }                                   // loops till temp node reaches the middle, then we return that node (i.e the middle node), which will print the subsequent linked list elements
-        return temp;
-    }
+        return slow;
+	}
 };
